26e770ffe052cddeed40e6ddfb0e89a88dda3175
Sergey Orlov <wladimirych@gmail.com>
2021-05-23 13:36:51 +0200

    switch from QtWebEngine to QtWebKit

--- qt_panellum/panellum.py
+++ qt_panellum/panellum.py
@@ -4,31 +4,36 @@ import json
 import binascii
 
 import pkg_resources
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
+from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtWebKit import QWebSettings
+
 from PyQt5.QtCore import QUrl
 
 
-class Panellum(QWebEngineView):
+class Panellum(QWebView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         settings = self.page().settings()
-        settings.setAttribute(QWebEngineSettings.LocalContentCanAccessRemoteUrls, True)
+        settings.setAttribute(QWebSettings.DeveloperExtrasEnabled, True)
+        settings.setAttribute(QWebSettings.LocalContentCanAccessFileUrls, False)
+        settings.setAttribute(QWebSettings.LocalContentCanAccessRemoteUrls, True)
+
         html_dir = pkg_resources.resource_filename(__name__, 'html')
         self.setUrl(QUrl('file://' + os.path.join(html_dir, 'index.html')))
 
     def eval_js(self, func, args=None):
-        return self.page().runJavaScript('%s(%s)' % (func, json.dumps(args)))
+        return self.page().mainFrame().evaluateJavaScript('%s(%s)' % (func, json.dumps(args)))
 
     def init_viewer(self, config):
         panorama_url = config.get('panorama', '')
         local_prefix = 'file://'
         if panorama_url.startswith(local_prefix):
             with open(panorama_url[len(local_prefix):], 'rb') as f:
-                config['panorama'] = 'data:;base64,' + binascii.b2a_base64(f.read()).decode('ascii')
+                config['panorama'] = 'data:image/jpeg;base64,' + binascii.b2a_base64(f.read()).decode('ascii')
         return self.eval_js('client.newViewer', config)
 
     def remove_viewer(self):
         self.eval_js('client.destroyViewer')
 
-    def viewer_command(self, method, args=None):
-        self.eval_js('client.viewerCommand', [method] + args or [])
+    def viewer_command(self, method, *args):
+        self.eval_js('client.viewerCommand', [method] + list(args))
